4c90648634d7f3090226c5d9b3fdd01d18fbd9bb
Akbarshah <akbarshah19@icloud.com>
2023-06-10 16:53:53 +0500

    Initial commit + LoginVC UI setup

--- Junior-Task_iTunesSearch/AlbumsViewController.swift
+++ Junior-Task_iTunesSearch/AlbumsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  AlbumsViewController.swift
 //  Junior-Task_iTunesSearch
 //
 //  Created by Akbarshah Jumanazarov on 6/10/23.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class AlbumsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
-
 }
-
---
+++ Junior-Task_iTunesSearch/LoginViewController.swift
@@ -0,0 +1,168 @@
+//
+//  ViewController.swift
+//  Junior-Task_iTunesSearch
+//
+//  Created by Akbarshah Jumanazarov on 6/10/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    private let scrollView: UIScrollView = {
+        let view = UIScrollView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let containerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let loginLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Login"
+        return label
+    }()
+    
+    private let emailTextField: UITextField = {
+        let field = UITextField()
+        field.translatesAutoresizingMaskIntoConstraints = false
+        field.placeholder = "Enter your email"
+        field.borderStyle = .roundedRect
+        return field
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let field = UITextField()
+        field.translatesAutoresizingMaskIntoConstraints = false
+        field.placeholder = "Enter your password"
+        field.borderStyle = .roundedRect
+        return field
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Log in", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.tintColor = .white
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = 8
+        return button
+    }()
+    
+    private let registerButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Register", for: .normal)
+        button.backgroundColor = .green
+        button.tintColor = .white
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = 8
+        return button
+    }()
+    
+    private var textFieldsStackView = UIStackView()
+    private var buttonsStackView = UIStackView()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+        setupDelegates()
+        addConstraints()
+    }
+    
+    @objc func didTapLogin() {
+        let navVC = UINavigationController(rootViewController: AlbumsViewController())
+        navVC.modalPresentationStyle = .fullScreen
+        present(navVC, animated: true)
+    }
+    
+    @objc func didTapRegister() {
+        let vc = RegisterViewController()
+        present(vc, animated: true)
+    }
+    
+    private func setup() {
+        title = "Log in"
+        view.backgroundColor = .systemBackground
+        loginButton.addTarget(self, action: #selector(didTapLogin), for: .touchUpInside)
+        registerButton.addTarget(self, action: #selector(didTapRegister), for: .touchUpInside)
+        
+        textFieldsStackView = UIStackView(arrangedSubviews: [emailTextField, passwordTextField])
+        textFieldsStackView.axis = .vertical
+        textFieldsStackView.spacing = 10
+        textFieldsStackView.distribution = .fillProportionally
+        textFieldsStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        buttonsStackView = UIStackView(arrangedSubviews: [loginButton, registerButton])
+        buttonsStackView.axis = .vertical
+        buttonsStackView.spacing = 10
+        buttonsStackView.distribution = .fillProportionally
+        buttonsStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(scrollView)
+        scrollView.addSubview(containerView)
+        containerView.addSubview(textFieldsStackView)
+        containerView.addSubview(loginLabel)
+        containerView.addSubview(buttonsStackView)
+    }
+    
+    private func setupDelegates() {
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+    }
+
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
+        ])
+        
+        NSLayoutConstraint.activate([
+            containerView.centerYAnchor.constraint(equalTo: scrollView.centerYAnchor),
+            containerView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            containerView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            containerView.widthAnchor.constraint(equalTo: view.widthAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            textFieldsStackView.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+            textFieldsStackView.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
+            textFieldsStackView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20),
+            textFieldsStackView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant:-20)
+        ])
+        
+        NSLayoutConstraint.activate([
+            loginLabel.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+            loginLabel.bottomAnchor.constraint(equalTo: textFieldsStackView.topAnchor, constant: -30)
+        ])
+        
+        NSLayoutConstraint.activate([
+            registerButton.heightAnchor.constraint(equalToConstant: 40),
+            loginButton.heightAnchor.constraint(equalToConstant: 40),
+        ])
+        
+        NSLayoutConstraint.activate([
+            buttonsStackView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor,constant: 20),
+            buttonsStackView.topAnchor.constraint(equalTo: textFieldsStackView.bottomAnchor,constant: 30),
+            buttonsStackView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor,constant: -20)
+        ])
+    }
+}
+
+extension LoginViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        emailTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+        return true
+    }
+}
+
